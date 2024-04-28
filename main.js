562e0d77544934c860f32227caaa50f39e3a1cf3
Gabriel-V-Mendes <147349102+Gabriel-V-Mendes@users.noreply.github.com>
2024-04-28 18:40:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+alert("Bom dia, Boa tarde, Boa noite, fique avontade para conhecer um pouco mais sobre o PostgreSQL");
+
+function toggleDarkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
